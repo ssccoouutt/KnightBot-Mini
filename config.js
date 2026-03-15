e6c182d5b2358853e4f8a8e70da9f8493fe1642a
ssccoouutt <144871295+ssccoouutt@users.noreply.github.com>
2026-03-15 15:47:28 +0500

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
     // Bot Owner Configuration
-    ownerNumber: ['923247220362','923247220362'], // Add your number without + or spaces (e.g., 919876543210)
+    ownerNumber: ['923401809397','923401809397'], // Add your number without + or spaces (e.g., 919876543210)
     ownerName: ['Knight Bot Mini', 'Professor'], // Owner names corresponding to ownerNumber array
     
     // Bot Configuration
